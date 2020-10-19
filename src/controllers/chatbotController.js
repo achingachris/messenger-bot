b19a258edb046f745865fb1d59bb6f32cd4952f7
ChrisAchinga <achinga.chris@gmail.com>
2020-10-19 15:04:49 +0300

    set hooks

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -6,6 +6,23 @@ let getWebHook = (req, res) => {}
 
 let postWebHook = (req, res) => {}
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 module.exports = {
   test: test,
+  getWebHook: getWebHook,
+  postWebHook: postWebHook
 };
