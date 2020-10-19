47ac2541afe030508be5be140580678965abe6be
ChrisAchinga <achinga.chris@gmail.com>
2020-10-19 14:49:29 +0300

    controller

---
+++ src/controllers/chatbotController.js
@@ -0,0 +1,11 @@
+let test = (req, res) => {
+  return res.send("Hello Again");
+};
+
+let getWebHook = (req, res) => {}
+
+let postWebHook = (req, res) => {}
+
+module.exports = {
+  test: test,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import chatbotController from "../controllers/chatbotController"
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello World");
-    })
+    router.get("/", chatbotController.test)
 
     return app.use("/", router); //setting defualt router to start with a slash
 }
